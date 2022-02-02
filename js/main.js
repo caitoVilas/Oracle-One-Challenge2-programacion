4a4f2a32c5936cf85f6372bc8e072e6d32856a2d
caito <caitocd@gmail.com>
2022-02-02 17:31:57 -0300

    finished game implementation

--- js/main.js
+++ js/main.js
@@ -1,113 +1,148 @@
 const d = document;
 const secretWords = ["CASA", "PERRO", "ALURA", "ORACLE", "JAVASCRIPT"];
-const word = secretWords[Math.floor(Math.random()* secretWords.length)];
-let hyphenatedWord = word.replace(/./g,"_ ");
-console.log(`${word} ${hyphenatedWord}`);
-const btnPress = d.getElementsByName('keyPress');
+let word;
+let hyphenatedWord;
+let letter;
+let contador = 0;
 
-       
 
-d.addEventListener("DOMContentLoaded", (e) => {
-    startGame("#btn-game");
-    addWord("#btn-add");
-    goHome(".btn-cancell");
-    getKey("btn-key");
+// captura elementos
+
+const $home = d.getElementById('home');
+const $game = d.getElementById('game');
+const $add  = d.getElementById('add-word');
+let $output = d.querySelector('#output');
+const $btnPress = d.getElementsByName('keyPress');
+const $msg = d.querySelector('#msg');
+const $msgErr = d.querySelector('#msg-err')
+const $ahorcado = d.querySelector('#horca');
+
+
+d.addEventListener('DOMContentLoaded', (e) => {
+
+    startGame('#btn-game');
+    addWord('#btn-add');
+    goHome('.btn-cancell');
+    newGame('.btn-new');
 });
 
+$btnPress.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        btn.classList.add('disable');
+        play(btn.innerText);
+    })
+})
+
+function play(letter){
+
+    let isFail = true;
+    
+    for(let i in word){
+
+        if(letter == word[i]){
+            hyphenatedWord = hyphenatedWord.replaceAt(i*2, letter);
+            isFail = false;
+        }
+    }
+
+    if(isFail){
+        contador++;
+        $ahorcado.src = `./images/${contador +1}.jpg`;
+        if(contador == 5){
+            $msgErr.innerHTML = `<i class="far fa-sad-tear 3x"></i><br>Has fallado!`
+        }
+        console.log("error")
+    }else{
+        $output.innerHTML = hyphenatedWord;
+        if(hyphenatedWord.indexOf('_') < 0){
+            $msg.innerHTML = `<i class="fas fa-trophy 3x"></i><br>Felicitaciones has ganado!`;
+        }
+    }
+}
 
 const startGame = (btnGame) => {
 
-   d.addEventListener('click', (e)=>{
-      
-       if(e.target.matches(btnGame)){
+    d.addEventListener('click',  (e) => {
 
-            let home = d.getElementById("home");
-            let game = d.getElementById("game");
-            let add = d.getElementById("add-word");
+        if(e.target.matches(btnGame)){
+
+            $home.classList.add('noView');
+            $add.classList.add('noView');
+            $game.classList.remove('noView');
+            initialize();
+            $output.innerHTML = hyphenatedWord;
             
-
-            home.classList.add("noView");
-            add.classList.add("noView");
-            game.classList.remove("noView");
-            d.querySelector('#output').innerHTML = hyphenatedWord;
-
-
-       }    
-
-   });
-};
-
+        }
+    });
+}
 
 const addWord = (btnAdd) => {
 
     d.addEventListener('click', (e) => {
-      
+
         if(e.target.matches(btnAdd)){
-            let home = d.getElementById("home");
-            let game = d.getElementById("game");
-            let add = d.getElementById("add-word")
 
-            home.classList.add("noView");
-            game.classList.add("noView");
-            add.classList.remove("noView");
-        }
-
-    });
-};
-
-const goHome = (btnCancell) => {
-
-    d.addEventListener('click', (e) => {
-
-        if(e.target.matches(btnCancell)){
-            let home = d.getElementById("home");
-            let game = d.getElementById("game");
-            let add = d.getElementById("add-word")
-
-            home.classList.remove("noView");
-            game.classList.add("noView");
-            add.classList.add("noView");
-
-            btnPress.forEach((button) => {
-                    button.classList.remove('disable');
-               
-            });
-            hyphenatedWord = word.replace(/./g,"_ ");
-
+            $home.classList.add('noView');
+            $game.classList.add('noView');
+            $add.classList.remove('noView');
         }
     });
-};
-
-const getKey = (btnKey) => {
-
-    d.addEventListener('click', (e) => {
-       
-        
-       
-        btnPress.forEach((button) => {
-            button.addEventListener('click', (e) => {
-                let letter = button.innerText;
-                button.classList.add('disable');
-           
-                for(let i in word){
-                    if(letter == word[i]){
-                       hyphenatedWord = hyphenatedWord.replaceAt(i*2, letter);
-                      
-                    }
-                    d.querySelector('#output').innerHTML = hyphenatedWord;
-                }
-                
-            });
-        });
-       
-    });
-   
 }
 
- function replaceAt(index, character){
-     return
- }
+const goHome = (btnCancel) => {
 
- String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + character + this.substr(index+character.length); } 
+    d.addEventListener('click', (e) => {
+
+        if(e.target.matches(btnCancel)){
+            $add.classList.add('noView');
+            $game.classList.add('noView');
+            $home.classList.remove('noView');
+            reset();
+        }
+    });
+}
+
+const initialize = () => {
+
+    word = secretWords[Math.floor(Math.random()* secretWords.length)];
+    hyphenatedWord = word.replace(/./g,"_ ");
+    console.log("initialize")
+    console.log(word)
+    console.log(hyphenatedWord)
+}
+
+const reset = () => {
+
+    word = ''
+    hyphenatedWord = '';
+    // console.log("reset");
+    // console.log(word);
+    // console.log(hyphenatedWord);
+    $btnPress.forEach((button) => {
+        button.classList.remove('disable');
+   
+    });
+    $msg.innerHTML = "";
+    $msgErr.innerHTML = "";
+    $ahorcado.src = './images/1.jpg';
+    contador = 0;
+}
+
+const newGame = (btnNew) => {
+
+    d.addEventListener('click', (e) => {
+
+        if(e.target.matches(btnNew)){
+            reset();
+            $home.classList.add('noView');
+            $add.classList.add('noView');
+            $game.classList.remove('noView');
+            initialize();
+            $output.innerHTML = hyphenatedWord;
+        }
+        
+    });
+}
 
 
+String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + character + this.substr(index+character.length); } 
